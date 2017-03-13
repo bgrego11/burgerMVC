61317812b1ad7f4cd117f56451434a0ceb2c7d06
makhno <makhno@makhnos-MacBook-Pro.local>
2017-03-12 20:07:52 -0500

    final

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('cheesy', true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('hot pepper', true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('banana', true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Jello', true);
+
